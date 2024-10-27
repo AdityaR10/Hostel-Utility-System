f154ab4d6927572fd5e1b8dd31f9a887b7538f86
Aditya Rajput <adityaudm@gmail.com>
2024-10-27 18:56:12 +0530

    File structure for root and home page

---
+++ app/(dashboard)/(routes)/admin/page.tsx
@@ -0,0 +1,10 @@
+const StudentDetailsPage = async () => { 
+
+    return ( 
+        <div className="p-6">
+            Admin page 
+        </div>
+     );
+}
+ 
+export default StudentDetailsPage;
